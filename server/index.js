6f5995f14495be9def43112332b42318e331aa86
Joseph <youssefbouryal02@gmail.com>
2026-01-13 01:10:19 +0100

    chore: add Docker setup for frontend and backend with Compose

--- server/index.js
+++ server/index.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 3042;
+const port = process.env.PORT || 3042;
 
 app.use(cors());
 app.use(express.json());
 
-// const balances = {
-//   "576eabd834cc87ed2877": 100,
-//   cc403c0ed9fd6bdb24fd: 50,
-//   "99130159a06169c18cd3": 75,
-// };
-
-// generate some addresses using wallet-gen.js script like the above
-const balances = {};
+const balances = {
+  "576eabd834cc87ed2877": 100,
+  cc403c0ed9fd6bdb24fd: 50,
+  "99130159a06169c18cd3": 75,
+};
 
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
